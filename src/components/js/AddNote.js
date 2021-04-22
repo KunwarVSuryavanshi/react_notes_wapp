8324bf3961823f05405d336237011458624b227c
VIRAJ SURYAVANSHI <k.viraj.suryavanshi@accenture.com>
2021-04-22 16:50:58 +0530

    Added forms

--- src/components/js/AddNote.js
+++ src/components/js/AddNote.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../css/AddNote.css'
 
 function AddNote() {
     return (
-        <div>
-            Add Note
+        <div className="add-note">
+            <form>
+                <input type="text" placeholder="Enter title"/>
+                <br/><br/><br/>
+                <textarea rows="12" cols="80"></textarea>
+                <br/><br/>
+                <input type="submit" value="Add Note" className="submit-bt"/>
+            </form>
         </div>
     )
 }
