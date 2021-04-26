c93f5102dd3fa3f5b5fea1a1d6e74fc6bed6a8ae
VIRAJ SURYAVANSHI <virajkunwar98@gmail.com>
2021-04-26 14:58:28 +0530

    be added

--- fe/src/components/js/AddNote.js
+++ fe/src/components/js/AddNote.js
@@ -1,26 +1,33 @@
 import React, { useState } from 'react'
 import '../css/AddNote.css'
-import bgg from '../images/bgg2.mp4'
-
+import bgg from '../images/ezgif.mp4'
+import axios from 'axios'
 function AddNote() {
     const [entry, setEntry] = useState({title:'',note:''})
     const handleChangeTitle = (event)=>{
         setEntry((entry ) => ({...entry, title:event.target.value}))
-        console.log(entry.title)
     }
     const handleChangeNotes = (event)=>{
         setEntry((entry)=>({...entry, note: event.target.value}))
     }
+    const handleSubmit = ()=>{
+       entry.preventDefault();
+        console.log(entry.title)
+        axios
+        .post('http://localhost:3001/add',)
+        .then()
+        .catch()
+    }
     return (
         <div className="add-note">
-            <video autoplay="autoplay" muted loop="loop" id="myVideo">
+            <video autoPlay="autoplay" muted loop="loop" id="myVideo">
                 <source src={bgg} type="video/mp4"/>
             </video>
             <div >
-            <form>
-                <input type="text" placeholder="Enter title" value={entry.title} onChange={handleChangeTitle}/>
+            <form onSubmit={handleSubmit}>
+                <input className="inp1" type="text" placeholder="Enter title" value={entry.title} onChange={handleChangeTitle}/>
                 <br/><br/><br/>
-                <textarea rows="12" cols="80" value={entry.note} onChange={handleChangeNotes}></textarea>
+                <textarea className="inp2" rows="12" cols="80" value={entry.note} onChange={handleChangeNotes}></textarea>
                 <br/><br/>
                 <input type="submit" value="Add Note" className="submit-bt"/>
             </form>
